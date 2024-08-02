c157dde570d4b6fb7a9e0e101c70e20ebffdbb7a
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-02 19:49:15 -0300

    Criação do `<Header  />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Card from './components/Card/Card'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <Card />
       <Card />
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+export default function Header() {
+    return (
+        <>
+            <div className='header'>
+                <h1>Eu sou um DevMon</h1>
+                <p>Os DevMons são criaturinhas resultantes da fusão de pokémons com tecnologias de software a partir da mente divagante do grande Paulo Salvatore - CEO da Salvatore Academy.</p>
+            </div>
+        </>
+    )
+}
